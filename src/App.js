1610aff3f65f86bc00417146f8f3f275d20cd0dc
Sean <writeseanhong@gmail.com>
2019-08-22 22:32:13 -0500

    commented and changed format

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 
-import Todoitem from "./components/Todoitem.js";
-import todoData from "./todoData";
+// import Todoitem from "./components/Todoitem.js";
+// import todoData from "./todoData";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      todos: todoData
-    };
-    this.handleChange = this.handleChange.bind(this);
-
-  };
-
-  handleChange(id) {
-    this.setState((prevState) => {
-      const updatedTodos = prevState.todos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      });
-      return {
-        todos: updatedTodos
-      }
-    })
-  }
-
+export default class App extends Component {
   render() {
-    const todos = this.state.todos.map(chore => <Todoitem key={chore.id} chore={chore} onChange={this.handleChange} />)
     return (
-      <div className="todo-list">
-        {todos}
+      <div>
+        This is the app component
       </div>
-    );
-  };
-};
+    )
+  }
+}
+
+
 
 // function App() {
 //   const chores = todoData.map(chore => <Todoitem chore={chore} />);
@@ -46,4 +24,41 @@ class App extends React.Component {
 //   );
 // }
 
-export default App;
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     todos: todoData
+  //   };
+  //   this.handleChange = this.handleChange.bind(this);
+
+  // };
+
+  // handleChange(id) {
+  //   this.setState((prevState) => {
+  //     const updatedTodos = prevState.todos.map(todo => {
+  //       if (todo.id === id) {
+  //         todo.completed = !todo.completed;
+  //       }
+  //       return todo;
+  //     });
+  //     return {
+  //       todos: updatedTodos
+  //     }
+  //   })
+  // }
+
+  // render() {
+  //   const todos = this.state.todos.map(chore => 
+  //     <Todoitem 
+  //       key={chore.id} 
+  //       chore={chore} 
+  //       onChange={this.handleChange} 
+  //     />)
+  //   return (
+  //     <div className="todo-list">
+  //       {todos}
+  //     </div>
+  //   );
+  // };
+
+
